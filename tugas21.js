4710d4bcd19b3504efe21b83b0bcbe6c8bc56154
bima rijaluddin <bimarijal@gmail.com>
2019-09-19 20:49:58 +0800

    Tugas 21 Javascript
    Tugas dari niomic

---
+++ tugas21.js
@@ -0,0 +1,8 @@
+function tugas21(){
+    var kalimat = "Saya belajar masak nasi goreng";
+    console.log(kalimat)
+    var hasil = kalimat.replace('masak','makan');
+    return hasil
+}
+
+console.log(tugas21());
